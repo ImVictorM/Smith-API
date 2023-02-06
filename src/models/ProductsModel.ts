6477499535a66275571b6317ed7d6831b00df51d
victor <victorutdsim7@gmail.com>
2023-02-06 15:31:26 -0300

    [req 1 - incompleto] feat: model rota post /products

---
+++ src/models/ProductsModel.ts
@@ -0,0 +1,21 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IProduct } from '../interfaces';
+
+export default class ProductsModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  createOneProducts = async (productFromReq: IProduct): Promise<IProduct> => {
+    const { name, amount } = productFromReq;
+    const query = 'INSERT INTO Trybesmith.products (name, amount) VALUES (?, ?)';
+    const [ headers ] = await this.connection.execute<ResultSetHeader>(query, [name, amount]);
+    const { insertId } = headers;
+    return {
+      id: insertId,
+      ...productFromReq,
+    };
+  }
+}
