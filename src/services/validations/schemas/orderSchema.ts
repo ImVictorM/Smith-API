09b5c9d8f82c0508c282be3f79b557f6a61ce16f
victor <victorutdsim7@gmail.com>
2023-02-07 16:11:14 -0300

    [req 8 - completo]: fix: mensagens de erro order schema

--- src/services/validations/schemas/orderSchema.ts
+++ src/services/validations/schemas/orderSchema.ts
@@ -1,7 +1,13 @@
 import Joi from 'joi';
 
 const orderReqPattern = Joi.object({
-  productsIds: Joi.array().items(Joi.number()).required(),
+  productsIds: Joi
+    .array()
+    .min(1)
+    .items(Joi.number())
+    .required().messages({
+      'array.min': '"productsIds" must include only numbers'
+    }),
 });
 
 export default orderReqPattern;
