58172ee5ea0366564a03d3a95a36ae3806779e4d
victor <victorutdsim7@gmail.com>
2023-02-07 15:41:42 -0300

    [req 8 - incompleto]: validação middleware de autenticação de usuário

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -7,9 +7,12 @@ const secret = process.env.JWT_SECRET as Secret;
 function authToken(req: Request, res: Response, next: NextFunction) {
   const { authorization: token } = req.headers;
   if (!token) {
-    return res.status(404).json({ message: 'Token not found' });
+    return res.status(401).json({ message: 'Token not found' });
   }
   const user = jwt.verify(token, secret, { algorithms: ['HS256'] }) as IUser;
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
   req.body = {
     ...req.body,
     userId: user.id,
